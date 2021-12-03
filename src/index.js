27da563d68979f522bfce9e9211712b54dfe38fe
rominalodolo <lodoloromina@gmail.com>
2021-12-03 11:32:58 +0200

    updates

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-// import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -12,9 +11,5 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// ReactDOM.render(<App />, document.getElementById('root')); registerServiceWorker();
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
