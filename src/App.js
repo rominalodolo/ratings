d5ebc56a372e9ed89767e9e3fcdd792d436a947a
rominalodolo <lodoloromina@gmail.com>
2021-12-06 12:41:03 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import Products from "./components/Products";
-import Rating from "./components/Rating";
+// import Rating from "./components/Rating";
 import { Button } from "react-bootstrap";
 import "./App.css";
 
@@ -10,12 +10,13 @@ class App extends Component {
       <div className='App'>
         <h1>My First React App: Product Listing</h1>
         <Button variant="primary" disabled>Click Me</Button>
-        <Rating rating="1" />
+        {"\n"}
+        {/* <Rating rating="1" />
         <Rating rating="2" />
         <Rating rating="3" />
         <Rating rating="4" />
-        <Rating rating="5" />
-        {/* <Products /> */}
+        <Rating rating="5" /> */}
+        <Products />
       </div>
     );
   }
